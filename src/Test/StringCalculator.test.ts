103c88fcefa9f94fe1f7545ce96038c02fec541f
Vivek Singh <vivek.singh@liqvid.com>
2024-09-20 23:20:46 +0530

    Dynamic delimiter

--- src/Test/StringCalculator.test.ts
+++ src/Test/StringCalculator.test.ts
@@ -5,25 +5,31 @@ describe('Basic Test',() => {
     test('Empty input test',() => {
         const sc = new StringCalculator();
         const res = sc.add('');
-        expect(res === 0);
+        expect(res).toEqual(0);
     });
 
     test('Single number test',() => {
         const sc = new StringCalculator();
         const res = sc.add('1');
-        expect(res === 1);
+        expect(res).toEqual(1);
     });
 
     test('Multiple number test',() => {
         const sc = new StringCalculator();
         const res = sc.add('1,5');
-        expect(res === 6);
+        expect(res).toEqual(6);
     });
 
     test('Newline input between number test',() => {
         const sc = new StringCalculator();
         const res = sc.add('1\n2,3');
-        expect(res === 6);
+        expect(res).toEqual(6);
+    });
+
+    test('Dynamic delimiter input test',() => {
+        const sc = new StringCalculator();
+        const res = sc.add('//;\n1;2');
+        expect(res).toEqual(3);
     });
 
 })
--- src/Utils/StringCalculator.ts
+++ src/Utils/StringCalculator.ts
@@ -1,24 +1,45 @@
-class StringCalculator{
+class StringCalculator {
 
-    private validateInput(numbers :string) :number{
-        if(numbers.length < 1){
+    private validateInput(numbers: string): number {
+        if (numbers.length < 1) {
             return 0
         }
         // throw new Error('Invalid Input')
         return 0;
     }
 
-    add(numbers :string):number{
+    add(numbers: string): number {
         this.validateInput(numbers);
-        const delimiter = ',';
-        
-        numbers = numbers.replaceAll('\n',',');
-        const numArr :string[] = numbers.split(delimiter);
+        const delimiter = this.getDelimiter(numbers);
+        console.log("🚀 ~ StringCalculator ~ add ~ delimiter:", delimiter)
+        numbers = numbers.replaceAll('\n', delimiter);
+        const numArr: string[] = numbers.split(delimiter);
+        return this.getTotal(numArr);
+    }
+
+    private getTotal(numArr: string[]): number {
         console.log("🚀 ~ StringCalculator ~ add ~ numArr:", numArr)
         let total = 0;
-        numArr.forEach((n) => total += parseInt(n))
-
+        numArr.forEach((n) => {
+            const tmp = parseInt(n);
+            console.log("🚀 ~ StringCalculator ~ numArr.forEach ~ tmp:", tmp)
+            if (!isNaN(tmp)) {
+                total += tmp;
+            }
+        });
+        console.log("🚀 ~ StringCalculator ~ getTotal ~ total:", total)
         return total;
+    }
+
+    private getDelimiter(numbers: string): string {
+        const delimiter = ',';
+        const delExpIndex = numbers.indexOf('//');
+        if (delExpIndex !== 0) { return delimiter; }
+        const newDelimiter = numbers[2];
+        if (newDelimiter != null && newDelimiter.length === 1) {
+            return newDelimiter;
+        }
+        return delimiter;
     }
 
 }
